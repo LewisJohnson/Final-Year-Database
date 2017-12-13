ca8867ca1c427148c4e71cbeaa86f597b5d8ca36
Lewis Johnson <lewisjohnsondev@gmail.com>
2017-12-13 20:51:14 +0000

    student factory

--- app/database/factories/StudentFactory.php
+++ app/database/factories/StudentFactory.php
@@ -1,23 +1,68 @@
 <?php
-
 use Faker\Generator as Faker;
 
-$autoIncrement = autoIncrement();
-$factory->define(SussexProjects\Student::class, function (Faker $faker) use ($autoIncrement) {
-	$autoIncrement->next();
-    return [
-    	'id' => $autoIncrement->current(),
-        'registration_number' => $faker->numberBetween(10000000,99999999),
-        'programme' => 'CompSci',
-        'project_status' => $faker->randomElement($array = array ('none','selected','proposed','accepted')),
-        'student_year' => $faker->randomElement($array = array ('final','masters')),
-        'share_project' => $faker->boolean($chanceOfGettingTrue = 70)
-    ];
+$studentIncrement = studentIncrement();
+$studentUgProjectIncrement = studentUgProjectIncrement();
+$studentMastersProjectIncrement = studentMastersProjectIncrement();
+
+$factory->define(SussexProjects\StudentUg::class, function (Faker $faker) use ($studentIncrement, $studentUgProjectIncrement) {
+	$studentIncrement->next();
+	$studentUgProjectIncrement->next();
+
+	$project_id = null;
+	$project_status = 'none';
+
+	if($studentUgProjectIncrement->current() <= 100){
+		$project_id = $studentUgProjectIncrement->current();
+		$project_status = $faker->randomElement($array = array ('selected', 'accepted'));
+	}
+
+	return [
+		'id' => $studentIncrement->current(),
+		'registration_number' => $faker->numberBetween(10000000,99999999),
+		'programme' => 'CompSci',
+		'project_status' => $project_status,
+		'project_id' => $project_id,
+		'share_project' => $faker->boolean($chanceOfGettingTrue = 70)
+	];
 });
 
-function autoIncrement()
-{
-    for ($i = 3; $i < 1000; $i++) {
-        yield $i;
-    }
+$factory->define(SussexProjects\StudentMasters::class, function (Faker $faker) use ($studentIncrement, $studentMastersProjectIncrement) {
+	$studentIncrement->next();
+	$studentMastersProjectIncrement->next();
+
+	$project_id = null;
+	$project_status = 'none';
+
+	if($studentMastersProjectIncrement->current() <= 100){
+		$project_id = $studentMastersProjectIncrement->current();
+		$project_status = $faker->randomElement($array = array ('selected', 'accepted'));
+	}
+
+	return [
+		'id' => $studentIncrement->current(),
+		'registration_number' => $faker->numberBetween(10000000,99999999),
+		'programme' => 'CompSci',
+		'project_status' => $project_status,
+		'project_id' => $project_id,
+		'share_project' => $faker->boolean($chanceOfGettingTrue = 70)
+	];
+});
+
+function studentIncrement(){
+	for ($i = 43; $i < 1000; $i++) {
+		yield $i;
+	}
+}
+
+function studentUgProjectIncrement(){
+	for ($i = 0; $i < 1000; $i++) {
+		yield $i;
+	}
+}
+
+function studentMastersProjectIncrement(){
+	for ($i = 0; $i < 1000; $i++) {
+		yield $i;
+	}
 }
