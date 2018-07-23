d9bc2367b370b781a28439d67f24a40172234e6b
Lewis Johnson <LewisJohnsonDev@gmail.com>
2018-07-23 11:54:49 +0100

    Bug Fix

--- app/app/Http/Requests/ProjectForm.php
+++ app/app/Http/Requests/ProjectForm.php
@@ -28,7 +28,8 @@ class ProjectForm extends FormRequest{
 		return [
 			'title' => 'required|max:255',
 			'description' => 'required|max:16777215',
-			'skills' => 'required|max:255', 'status' => 'required',
+			'skills' => 'required|max:255',
+			'status' => 'required',
 		];
 	}
 
@@ -36,7 +37,7 @@ class ProjectForm extends FormRequest{
 		return [
 			'title' => 'required|max:255',
 			'description' => 'required|max:16777215',
-			'skills' => 'required|max:255', 'status' => 'required',
+			'skills' => 'required|max:255'
 		];
 	}
 }
