854965fa0ed7b3a240646870d4b2ef400d450581
Lewis Johnson <lewisjohnsondev@gmail.com>
2018-08-19 16:18:41 +0100

    Updated search to exclude empty terms

--- app/resources/assets/js/views/supervisor-report.js
+++ app/resources/assets/js/views/supervisor-report.js
@@ -21,20 +21,25 @@
 	});
 
 	function scrollToSupervisor(){
+		if(input.val() === '' || !input.val().replace(/\s/g, '').length){
+			createToast('error', 'Invalid search term.');
+			return;
+		}
+
 		var supervisor = $('#' +  input.val().replace(/[\s.]+/g, ''));
 
 		if(supervisor.length < 1){
-			createToast("error", 'Supervisor "' + input.val() + '" can not be found.');
+			createToast('error', 'Supervisor "' + input.val() + '" can not be found.');
 		} else {
-			supervisor.removeClass("animated shake");
-			$("html, body").animate({
-				scrollTop: supervisor.offset().top - 150 }, 600,
+			supervisor.removeClass("animated flash");
+			$("html, body").animate({ scrollTop: supervisor.offset().top - 150 }, 600,
 				function() {
-					supervisor.addClass("animated shake");
+					supervisor.addClass("animated flash");
 					supervisor.focus();
-			});
+				}
+			);
 		}
 
-		input.val("");
+		input.val('');
 	}
 });
