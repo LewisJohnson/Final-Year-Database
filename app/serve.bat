94ef3eef950709fa5b852c97ffa3d47a55e456c4
Lewis Johnson <lewisjohnsondev@gmail.com>
2018-09-08 13:54:33 +0100

    Fixed junk

--- app/serve.bat
+++ app/serve.bat
@@ -3,5 +3,4 @@ cd C:\xampp\htdocs\Final-Year-Database\app
 
 start cmd.exe @cmd /k "php artisan serve"
 start cmd.exe @cmd /k "npm run watch"
-start cmd.exe @cmd /k "ngrok http -region eu 8000"
 start cmd.exe @cmd /k
