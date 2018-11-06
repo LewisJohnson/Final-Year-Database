ef27423df02bd8dad967a586da3ebcb2cb8376d3
Lewis Johnson <lewisjohnsondev@gmail.com>
2018-11-06 16:14:57 +0000

    Improved project popularity

--- app/resources/assets/js/views/project-popularity.js
+++ app/resources/assets/js/views/project-popularity.js
@@ -11,36 +11,89 @@ import WordCloud from '../wordcloud';
 
 	// Declared at the top of view.
 	if(Window["pageEnabled"]){
-		var projects = new Array();
+
+		if(!WordCloud.isSupported){
+			$('.content').text('Sorry, word cloud is not supported on your device.');
+			return;
+		}
+
+		WordCloud.minFontSize = 12;
+
+		$('.loader').show();
+		$('.content').hide();
+
+		var titleArray = [];
+		var titleAndDescArray = [];
 
 		// Get all the data for the cloud using the table
 		$('.project-row').each(function(index) {
-			// Temp array to put into other array
-			var project = new Array();
-			var title = $(this).find('.title').text();
-			project.push(title);
-			project.push(11 - index);
+			var titleWords = $(this).data('title').toLowerCase().split(" ");
 
-			// Push to main array
-			projects.push(project);
+			for(var i = 0; i < titleWords.length; i++)
+			{
+				var matchedWord = titleArray.find(function(ele) {
+					return ele[0] === titleWords[i];
+				});
+
+				if (matchedWord == null) {
+					titleArray.push(new Array(titleWords[i], 1));
+				} else {
+					matchedWord[1] += 1;
+				}
+			}
+
+			var descriptionWords = $(this).data('description').toLowerCase().split(" ");
+			for(var i = 0; i < descriptionWords.length; i++)
+			{
+				var matchedWord = titleAndDescArray.find(function(ele) {
+					return ele[0] === descriptionWords[i];
+				});
+
+				if (matchedWord == null) {
+					titleAndDescArray.push(new Array(descriptionWords[i], 1));
+				} else {
+					matchedWord[1] += 1;
+				}
+			}
 		});
 
-		if(!WordCloud.isSupported){
-			$('#word-cloud').text('Sorry, word cloud is not supported on your device.')
-		}
-
-		var width = $('#word-cloud').width();
+		var titleAndDescArray = titleAndDescArray.concat(titleArray);
 
 		var options = {
-			list: projects,
-			weightFactor: function (size) {
-				return size * width / 256;
-			},
+			list: titleAndDescArray,
+			weightFactor: 30,
 			rotateRatio: 0.5,
 			rotationSteps: 2,
-			font: 'Roboto, sans-serif',
+			color : '#1D4289',
+			minSize: 12,
+			font: 'Roboto, sans-serif'
+		}
+
+
+		var titleOptions = {
+			list: titleArray,
+			weightFactor: 30,
+			rotateRatio: 0.5,
+			rotationSteps: 2,
+			color : '#1D4289',
+			minSize: 12,
+			font: 'Roboto, sans-serif'
 		}
 
 		WordCloud($('#word-cloud')[0], options);
+		WordCloud($('#word-cloud-title-only')[0], titleOptions);
+
+		$('#word-cloud-exclude-description').on('change', function(){
+			if($(this).is(':checked')){
+				$('#word-cloud').hide();
+				$('#word-cloud-title-only').show();
+			} else {
+				$('#word-cloud').show();
+				$('#word-cloud-title-only').hide();
+			}
+		});
+
+		$('.loader-container').remove();
+		$('.content').fadeIn(config.animtions.slow);
 	}
 });
--- app/resources/views/supervisors/project-popularity.blade.php
+++ app/resources/views/supervisors/project-popularity.blade.php
@@ -24,27 +24,51 @@
 	@else
 		<div class="centered width--800">
 			<h1>Project Popularity</h1>
-			<p class="subtitle">Your {{ count($projects) }} most popular on-offer {{ Session::get('education_level')["longName"] }} projects viewed by students.</p>
 
-			<table class="data-table table--dark-head sort-table">
-				<thead>
-					<tr>
-						<th class="pointer">Project</th>
-						<th title="A view is counted when a student vists one of your project pages for longer than 5 seconds." class="pointer"><span class="dashed-underline">View Count</span></th>
-					</tr>
-				</thead>
-				<tbody>
-					@foreach($projects as $project)
-						<tr class="project-row">
-							<td><a class="title" href="{{ action('ProjectController@show', $project->id) }}">{{ $project->title }}</a></td>
-							<td class="views">{{ $project->view_count }}</td>
+			<div class="loader-container">
+				<div class="loader loader--large"></div>
+				<p>One moment, the numbers are being crunched...</p>
+			</div>
+
+			<div class="content">
+				<p class="subtitle">Your {{ count($projects) }} most popular on-offer {{ Session::get('education_level')["longName"] }} projects viewed by students.</p>
+
+				<table class="data-table table--dark-head sort-table">
+					<thead>
+						<tr>
+							<th class="pointer">Project</th>
+							<th title="A view is counted when a student vists one of your project pages for longer than 5 seconds." class="pointer"><span class="dashed-underline">View Count</span></th>
 						</tr>
-					@endforeach
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						@foreach($projects as $project)
+							<tr class="project-row" data-title="{{ $project->title }}" data-description="{{ $project->description }}">
+								<td><a href="{{ action('ProjectController@show', $project->id) }}">{{ $project->title }}</a></td>
+								<td>{{ $project->view_count }}</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
 
-			<h2>Word Cloud</h2>
-			<div id="word-cloud" style="height: 800px"></div>
+				<hr style="margin: 20px 0 ">
+
+				<h2>Word Cloud</h2>
+
+				<div class="flex flex--row flex--stretch-children">
+					<span>The Word Cloud shows the most used words in your projects.</span>
+
+					<div class="form-field--toggle" style="text-align: right">
+						<p class="switch-label" style="top: 0" >Exclude descriptions</p>
+						<label class="toggle">
+							<input type="checkbox" id="word-cloud-exclude-description" class="boolean-checkbox">
+							<span class="slider"></span>
+						</label>
+					</div>
+				</div>
+
+				<canvas width="800" height="400" id="word-cloud"></canvas>
+				<canvas width="800" height="400" hidden id="word-cloud-title-only"></canvas>
+			</div>
 		</div>
 	@endif
 @endsection
