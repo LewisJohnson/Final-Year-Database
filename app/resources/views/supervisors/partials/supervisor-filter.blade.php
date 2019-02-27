de3bdb6fcbf22380d08fdc7db61d1a5503ffc7da
Lewis Johnson <lewisjohnsondev@gmail.com>
2019-02-27 07:36:16 +0000

    Upgraded supervisor filter to BS4

--- app/resources/views/supervisors/partials/supervisor-filter.blade.php
+++ app/resources/views/supervisors/partials/supervisor-filter.blade.php
@@ -1,4 +1,4 @@
 {!! SussexProjects\Supervisor::getDatalist() !!}
-<div class="search-container shadow-4dp">
-	<input id="supervisor-report-search-input" list="supervisor-datalist" class="search-input" style="flex-grow: 1;" type="text" placeholder="Start typing to filter supervisors...">
+<div class="search-container w-100 shadow-sm mt-3">
+	<input id="supervisor-report-search-input" list="supervisor-datalist" class="w-100 search-input" type="text" placeholder="Start typing to filter supervisors...">
 </div>
