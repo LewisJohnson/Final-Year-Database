13aa5de2b0cf55e9df5640d580cd801bcb131e21
Lewis Johnson <lewisjohnsondev@gmail.com>
2019-02-27 12:35:11 +0000

    Updated some cookie names to prevent clashing

--- app/app/Http/Middleware/RequiredCookies.php
+++ app/app/Http/Middleware/RequiredCookies.php
@@ -21,20 +21,35 @@ class RequiredCookies{
 	 * @return mixed
 	 */
 	public function handle($request, Closure $next){
+
+		// View: All
+		// Action: Toggles accessible font
 		if(Cookie::get('accessibility_font') == null){
 			Cookie::queue('accessibility_font', false, 525600);
 		}
 
+		// View: All
+		// Action: Toggles accessible contrast
 		if(Cookie::get('accessibility_contrast') == null){
 			Cookie::queue('accessibility_contrast', false, 525600);
 		}
 
+		// View: Home (Student), Project (Student)
+		// Action: A list of favourite projects
 		if(empty(Cookie::get('favourite_projects'))){
 			Cookie::queue('favourite_projects', null, 525600);
 		}
 
-		if(Cookie::get('hide_archived') == null){
-			Cookie::queue('hide_archived', false, 525600);
+		// View: My Projects 
+		// Action: Toggles the display of archived projects
+		if(Cookie::get('mp_hide_archived') == null){
+			Cookie::queue('mp_hide_archived', true, 525600);
+		}
+
+		// View: My Projects 
+		// Action: Toggles the display of archived projects
+		if(Cookie::get('sr_hide_closed') == null){
+			Cookie::queue('sr_hide_closed', true, 525600);
 		}
 
 		return $next($request);
