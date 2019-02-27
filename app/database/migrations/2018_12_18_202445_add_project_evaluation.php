c60729508fc9f92662aa1cc451f30220a1d92200
Lewis Johnson <lewisjohnsondev@gmail.com>
2019-02-27 12:36:22 +0000

    Added project evaluation migration

---
+++ app/database/migrations/2018_12_18_202445_add_project_evaluation.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProjectEvaluation extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		foreach(get_departments() as $key => $department) {
+			foreach(get_education_levels() as $key => $level) {
+				Schema::create($department.'_project_evaluation_'.$level['shortName'], function (Blueprint $table) use ($department, $level){
+					$table->uuid('id')->unqiue();
+					$table->uuid('project_id');
+					$table->boolean('is_finalised');
+					$table->text('questions');
+
+					$table->primary('id');
+					$table->foreign('project_id')->references('id')->on($department.'_projects_'.$level['shortName'])->onDelete('CASCADE');
+				});
+
+				DB::statement("ALTER TABLE `".$department.'_project_evaluation_'.$level['shortName']."` ADD `project_year` YEAR NOT NULL;");
+			}
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		foreach(get_departments() as $key => $department) {
+			foreach(get_education_levels() as $key => $level) {
+				Schema::table($department.'_project_evaluation_'.$level['shortName'], function (Blueprint $table) use ($department, $level){
+					$table->dropForeign($department.'_project_evaluation_'.$level['shortName'].'_project_id_foreign');
+				});
+
+				Schema::drop($department.'_project_evaluation_'.$level['shortName']);
+			}
+		}
+	}
+}
