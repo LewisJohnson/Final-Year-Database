8c721268fea09fc913c1925669965f6554cf786d
Lewis Johnson <lewisjohnsondev@gmail.com>
2019-02-27 12:21:08 +0000

    Renamed byTopics and bySupervisor view files to be improve searchability
    - Upgraded by topics to BS4
    - Upgraded by supervisor to BS4

--- app/resources/views/projects/by-supervisor.blade.php
+++ app/resources/views/projects/by-supervisor.blade.php
@@ -3,22 +3,29 @@
 @section('content')
 <div class="centered mw-800">
 	<h1>Projects by Supervisor</h1>
-	<h3>Select a supervisor to see their projects.</h3>
-	@include('supervisors.partials.supervisor-filter')
+	<h5>Select a supervisor to see their projects</h5>
 
-	<table class="data-table sort-table shadow-2dp">
+	<div class="mt-4">
+		@include('supervisors.partials.supervisor-filter')
+	</div>
+
+	<table class="table table-hover bg-white  data-table sort-table shadow-2dp mt-4">
 		<thead>
 			<tr>
 				<th>Supervisor</th>
-				<th>Projects</th>
+				<th class="text-right">Projects</th>
 			</tr>
 		</thead>
 		<tbody>
 			@foreach($supervisors as $supervisor)
-				@if(count($supervisor->getProjects('on-offer')) > 0)
+				@php
+					$projectsOnOfferCount = count($supervisor->getProjects('on-offer'));
+				@endphp
+
+				@if($projectsOnOfferCount > 0)
 					<tr class="cursor--pointer supervisor-row" data-supervisor-name="{{ $supervisor->user->getFullName() }}" onclick="window.location='{{ action('UserController@projects', $supervisor->user)}}';">
 						<td>{{ $supervisor->user->getFullName() }}</td>
-						<td>{{ count($supervisor->getProjects('on-offer')) }}</td>
+						<td class="text-right">{{ $projectsOnOfferCount }}</td>
 					</tr>
 				@endif
 			@endforeach
--- app/resources/views/projects/by-topic.blade.php
+++ app/resources/views/projects/by-topic.blade.php
@@ -2,12 +2,13 @@
 @section ('content')
 <div class="centered mw-800">
 	<h1>Projects by Topic</h1>
-	<h3>Select a topic to browse related projects.</h3>
-	<table class="data-table sort-table shadow-2dp">
+	<h5>Select a topic to browse related projects.</h5>
+	
+	<table class="table table-hover bg-white data-table mt-3 sort-table shadow-sm">
 		<thead>
 			<tr>
-				<th class="cursor--pointer">Topic</th>
-				<th class="cursor--pointer">Projects</th>
+				<th>Topic</th>
+				<th class="text-right">Projects</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -19,7 +20,7 @@
 			@if($amountOfProjects > 0)
 				<tr tabindex="0" class="cursor--pointer" onclick="window.location='{{ action('ProjectController@byTopic', $topic->id)}}';">
 					<td>{{ $topic->name }}</td>
-					<td>{{ $amountOfProjects }}</td>
+					<td class="text-right">{{ $amountOfProjects }}</td>
 				</tr>
 			@endif
 		@endforeach
