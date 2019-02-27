8d4d942e9e7fa1148ecb0ca2aaa95d213fdded7d
Lewis Johnson <lewisjohnsondev@gmail.com>
2019-02-27 12:37:08 +0000

    Added copy action to transaction table

---
+++ app/database/migrations/2019_02_11_090933_add_copy_action_to_transaction_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCopyActionToTransactionTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		foreach(get_departments() as $key => $department) {
+			foreach(get_education_levels() as $key => $level) {
+				$transactionTableName = $department.'_transactions_'.$level['shortName'];
+
+				Schema::table($transactionTableName, function (Blueprint $table) use ($transactionTableName) {
+					DB::statement("ALTER TABLE `".$transactionTableName."` CHANGE COLUMN `action` `action` ENUM('proposed','selected','accepted','rejected','deleted','updated','created','undo','marker-assigned','copy') NOT NULL");
+				});
+			}
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down(){
+		foreach(get_departments() as $key => $department) {
+			foreach(get_education_levels() as $key => $level) {
+				$transactionTableName = $department.'_transactions_'.$level['shortName'];
+
+				Schema::table($transactionTableName, function (Blueprint $table) use ($transactionTableName) {
+					DB::statement("ALTER TABLE `".$transactionTableName."` CHANGE COLUMN `action` `action` ENUM('proposed','selected','accepted','rejected','deleted','updated','created','undo','marker-assigned') NOT NULL");
+				});
+			}
+		}
+	}
+}
