fedd0f45453ad387dca98be352ef3bef65e44974
Lewis Johnson <lewisjohnsondev@gmail.com>
2019-02-27 12:49:00 +0000

    Updated project preview to BS4

--- app/resources/views/projects/partials/student-project-preview.blade.php
+++ app/resources/views/projects/partials/student-project-preview.blade.php
@@ -1,27 +1,28 @@
 <div class="project-preview project-preview--student">
 	<div class="project">
 		
-		<h1 class="title">{{ $project->title }}</h1>
+		<h2 class="text-capitalize text-center">{{ $project->title }}</h2>
 
 		@if($project->status == "student-proposed")
-			<h2 class="supervisor">You proposed this project to {{ $project->supervisor->user->getFullName() }}</h2>
+			<h4 class="text-center text-muted">Proposed to {{ $project->supervisor->user->getFullName() }}</h3>
 		@else
-			<h2 class="supervisor">{{ $project->supervisor->user->getFullName() }}</h2>
+			<h4 class="text-capitalize text-center text-muted">{{ $project->supervisor->user->getFullName() }}</h3>
 		@endif
 
-		<h3>Description</h3>
+		<h5>Description</h5>
 		<div>
 			<p>{!! html_entity_decode($project->description) !!}</p>
 		</div>
 
-		<h3>Skills</h3>
+		<h5>Skills</h5>
 		<p>{{ $project->skills }}</p>
-		<h3>Topics</h3>
+
+		<h5>Topics</h5>
 		<ul class="topics-list">
 			@if (count($project->topics))
 				@foreach($project->topics as $topic)
 					@if($project->getPrimaryTopic())
-						<li class="cursor--pointer topic{!! ($topic->id == $project->getPrimaryTopic()->id) ? ' primary first': '' !!}" draggable onclick="window.location='{{ action('ProjectController@byTopic', $topic->id) }}';">
+						<li class="cursor--pointer topic{!! ($topic->id == $project->getPrimaryTopic()->id) ? ' primary first': '' !!}" onclick="window.location='{{ action('ProjectController@byTopic', $topic->id) }}';">
 							<p>{{$topic->name}}</p>
 						</li>
 					@else
@@ -32,21 +33,13 @@
 				@endforeach
 			@endif
 			@if(!count($project->topics))
-				<li class="no-topics">
-					<svg style="width:24px;height:24px;position: relative;top: 5px;" viewBox="0 0 24 24">
-						<path fill="#fff" d="M13,14H11V10H13M13,18H11V16H13M1,21H23L12,2L1,21Z" />
-					</svg>
-					<p>This project has no associated topics.</p>
-				</li>
-			@elseif(!$project->getPrimaryTopic())
-				<li class="no-topics">
-					<svg style="width:24px;height:24px;position: relative;top: 5px;" viewBox="0 0 24 24">
-						<path fill="#fff" d="M13,14H11V10H13M13,18H11V16H13M1,21H23L12,2L1,21Z" />
-					</svg>
-					<p>This project has no primary topic.</p>
+				<li class="text-muted">
+					This project has no associated topic(s).
 				</li>
 			@endif
 		</ul>
 	</div>
-	<a class="show-more" href="#show">Show more &raquo;</a>
+	<div class="text-center">
+		<a id="expand-student-project-preview" class="text-center w-100 btn text-dark" href="#expand">Show more ↓</a>
+	</div>
 </div>
