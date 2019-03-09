570a1a223d502202169835ec7cf97da51559cb37
Lewis Johnson <LewisJohnsonDev@gmail.com>
2019-03-09 12:33:08 +0000

    Updated login to use BS4
    - Added department to the title of the login page

--- app/resources/views/auth/login.blade.php
+++ app/resources/views/auth/login.blade.php
@@ -2,43 +2,42 @@
 	@include('partials.html-head')
 	@include('partials.header')
 
-	<div class="mw-600 centered card" style="margin-top: 3rem;">
-		<div class="header">
-			<h2 id="dialog-title">LOG IN</h2>
-			<p id="dialog-desc">Use your Sussex ITS login details to log in to this system</p>
-		</div>
-
-		<div class="content">
-			<form id="loginForm" class="form d-flex" role="form" method="POST" action="{{ action('Auth\LoginController@login')}}" accept-charset="utf-8">
-				{{ csrf_field() }}
-
-				<div type="email" autocorrect="off" autocapitalize="none" class="form-field">
-					<label for="username">Username/Email</label>
-					<input id="username"  type="text" name="username" value="{{ old('username') }}" required autofocus>
-				</div>
-
-				<div class="form-field">
-					<label for="password">Password</label>
-					<input id="password" type="password" name="password" required>
-				</div>
-
-				<div class="form-field" title="This is not recommended for shared devices">
-					<div class="checkbox">
-						<input id="remember" name="title" type="checkbox" {{ old('remember') ? 'checked' : '' }}>
-						<label for="remember">Remember Me</label>
+	<div class="mw-600 centered mt-3">
+		<div class="card">
+			<div class="card-header text-uppercase">{{ Session::get('department') }} LOG IN</div>
+		
+			<div class="card-body">
+				<form id="loginForm" class="form" role="form" method="POST" action="{{ action('Auth\LoginController@login')}}" accept-charset="utf-8">
+					{{ csrf_field() }}
+	
+					<div type="email" autocorrect="off" autocapitalize="none" class="form-field">
+						<label for="username">Username/Email</label>
+						<input id="username"  type="text" name="username" value="{{ old('username') }}" required autofocus>
 					</div>
-				</div>
-
-				<p class="help-block" style="display: none;"></p>
-				<div id="redirect-block" style="display: none;">
-					<h3 style="margin-bottom: 5px">After login, you will be redirected to</h3>
-					<p style="margin-top: 0"></p>
-				</div>
-
-				<div class="flex flex--row">
-					<button class="button button--raised button--accent" type="submit">LOG IN</button>
-				</div>
-			</form>
+	
+					<div class="form-field">
+						<label for="password">Password</label>
+						<input id="password" type="password" name="password" required>
+					</div>
+	
+					<div class="form-field" title="This is not recommended for shared devices">
+						<div class="checkbox">
+							<input id="remember" name="title" type="checkbox" {{ old('remember') ? 'checked' : '' }}>
+							<label for="remember">Remember Me</label>
+						</div>
+					</div>
+	
+					<p class="help-block" style="display: none;"></p>
+					<div id="redirect-block" style="display: none;">
+						<h3 style="margin-bottom: 5px">After login, you will be redirected to</h3>
+						<p style="margin-top: 0"></p>
+					</div>
+	
+					<div class="text-right mt-3">
+						<button class="btn btn-primary" type="submit">LOG IN</button>
+					</div>
+				</form>
+			</div>
 		</div>
 	</div>
 
@@ -51,7 +50,7 @@
 @else
 	<div id="login-dialog" data-type="ajax" class="dialog login" data-dialog="login">
 		<div class="border-bottom">
-			<h4 id="dialog-title" class="text-center p-3 m-0 font-weight-bold">LOG IN</h4>
+			<h4 id="dialog-title" class="text-center p-3 m-0 font-weight-bold text-uppercase">{{ Session::get('department') }} LOG IN</h4>
 			<p id="dialog-desc" hidden>Use your Sussex ITS login details to log in to this system</p>
 		</div>
 
