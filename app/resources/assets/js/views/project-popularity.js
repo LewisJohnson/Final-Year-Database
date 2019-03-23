92cb034d74e2e409e83bc13f99539395fbece01f
Lewis Johnson <LewisJohnsonDev@gmail.com>
2019-03-24 00:23:39 +0100

    Removed project popularity requirments

--- app/resources/assets/js/views/project-popularity.js
+++ app/resources/assets/js/views/project-popularity.js
@@ -10,90 +10,89 @@ import WordCloud from '../wordcloud';
 	"use strict";
 
 	// Declared at the top of view.
-	if(Window["pageEnabled"]){
 
-		if(!WordCloud.isSupported){
-			$('.content').text('Sorry, word cloud is not supported on your device.');
-			return;
-		}
-
-		WordCloud.minFontSize = 12;
-
-		$('.loader').show();
-		$('.content').hide();
-
-		var titleArray = [];
-		var titleAndDescArray = [];
-
-		// Get all the data for the cloud using the table
-		$('.project-row').each(function(index) {
-			var titleWords = $(this).data('title').toString().toLowerCase().split(" ");
-
-			for(var i = 0; i < titleWords.length; i++)
-			{
-				var matchedWord = titleArray.find(function(ele) {
-					return ele[0] === titleWords[i];
-				});
-
-				if (matchedWord == null) {
-					titleArray.push(new Array(titleWords[i], 1));
-				} else {
-					matchedWord[1] += 1;
-				}
-			}
-
-			var descriptionWords = $(this).data('description').toLowerCase().split(" ");
-			for(var i = 0; i < descriptionWords.length; i++)
-			{
-				var matchedWord = titleAndDescArray.find(function(ele) {
-					return ele[0] === descriptionWords[i];
-				});
-
-				if (matchedWord == null) {
-					titleAndDescArray.push(new Array(descriptionWords[i], 1));
-				} else {
-					matchedWord[1] += 1;
-				}
-			}
-		});
-
-		var titleAndDescArray = titleAndDescArray.concat(titleArray);
-
-		var options = {
-			list: titleAndDescArray,
-			weightFactor: 30,
-			rotateRatio: 0.5,
-			rotationSteps: 2,
-			color : '#1D4289',
-			minSize: 12,
-			font: 'Roboto, sans-serif'
-		}
-
-
-		var titleOptions = {
-			list: titleArray,
-			weightFactor: 30,
-			rotateRatio: 0.5,
-			rotationSteps: 2,
-			color : '#1D4289',
-			minSize: 12,
-			font: 'Roboto, sans-serif'
-		}
-
-		WordCloud($('#word-cloud')[0], options);
-		WordCloud($('#word-cloud-title-only')[0], titleOptions);
-
-		$('#word-cloud-exclude-description').on('change', function(){
-			if($(this).is(':checked')){
-				$('#word-cloud').hide();
-				$('#word-cloud-title-only').show();
-			} else {
-				$('#word-cloud').show();
-				$('#word-cloud-title-only').hide();
-			}
-		});
-
-		$('.loader-container').remove();
-		$('.content').fadeIn(config.animtions.slow);
+	if(!WordCloud.isSupported){
+		$('.content').text('Sorry, word cloud is not supported on your device.');
+		return;
 	}
+
+	WordCloud.minFontSize = 12;
+
+	$('.loader').show();
+	$('.content').hide();
+
+	var titleArray = [];
+	var titleAndDescArray = [];
+
+	// Get all the data for the cloud using the table
+	$('.project-row').each(function(index) {
+		var titleWords = $(this).data('title').toString().toLowerCase().split(" ");
+
+		for(var i = 0; i < titleWords.length; i++)
+		{
+			var matchedWord = titleArray.find(function(ele) {
+				return ele[0] === titleWords[i];
+			});
+
+			if (matchedWord == null) {
+				titleArray.push(new Array(titleWords[i], 1));
+			} else {
+				matchedWord[1] += 1;
+			}
+		}
+
+		var descriptionWords = $(this).data('description').toLowerCase().split(" ");
+		for(var i = 0; i < descriptionWords.length; i++)
+		{
+			var matchedWord = titleAndDescArray.find(function(ele) {
+				return ele[0] === descriptionWords[i];
+			});
+
+			if (matchedWord == null) {
+				titleAndDescArray.push(new Array(descriptionWords[i], 1));
+			} else {
+				matchedWord[1] += 1;
+			}
+		}
+	});
+
+	var titleAndDescArray = titleAndDescArray.concat(titleArray);
+
+	var options = {
+		list: titleAndDescArray,
+		weightFactor: 30,
+		rotateRatio: 0.5,
+		rotationSteps: 2,
+		color : '#1D4289',
+		minSize: 12,
+		font: 'Roboto, sans-serif'
+	}
+
+
+	var titleOptions = {
+		list: titleArray,
+		weightFactor: 30,
+		rotateRatio: 0.5,
+		rotationSteps: 2,
+		color : '#1D4289',
+		minSize: 12,
+		font: 'Roboto, sans-serif'
+	}
+
+	WordCloud($('#word-cloud')[0], options);
+	WordCloud($('#word-cloud-title-only')[0], titleOptions);
+
+	$('#word-cloud-exclude-description').on('change', function(){
+		if($(this).is(':checked')){
+			$('#word-cloud').hide();
+			$('#word-cloud-title-only').show();
+		} else {
+			$('#word-cloud').show();
+			$('#word-cloud-title-only').hide();
+		}
+	});
+
+	$('.loader-container').remove();
+	$('.content').fadeIn(config.animtions.slow);
+
 });
--- app/resources/views/supervisors/project-popularity.blade.php
+++ app/resources/views/supervisors/project-popularity.blade.php
@@ -1,74 +1,56 @@
 @extends('layouts.app')
 
 @section('scripts')
-	<script type="text/javascript">
-		Window["pageEnabled"] = {{ count($projects) >= 3 ? 'true' : 'false' }};
-	</script>
 	<script src="{{ asset('js/views/project-popularity.js') }}"></script>
 @endsection
 
 @section('content')
-	@if(count($projects) < 3)
-		<div class="centered mw-800">
-			<div class="fancy-page">
-				<h1>Project Popularity</h1>
-				<p class="subtitle">We need to gather a little bit more data before showing you this page, check back soon.</p>
+	<div class="centered mw-800">
+		<h1>Project Popularity</h1>
 
-				<div id="cogs">
-					<div class="cog cog--large">@include('svg.cog')</div>
-					<div class="cog cog--medium">@include('svg.cog')</div>
-					<div class="cog cog--small">@include('svg.cog')</div>
-				</div>
-			</div>
+		<div class="loader-container">
+			<div class="spinner spinner-grow"></div>
+			<p>One moment, the numbers are being crunched...</p>
 		</div>
-	@else
-		<div class="centered mw-800">
-			<h1>Project Popularity</h1>
 
-			<div class="loader-container">
-				<div class="loader loader--large"></div>
-				<p>One moment, the numbers are being crunched...</p>
-			</div>
+		<div class="content">
+			<p class="subtitle">Your {{ count($projects) }} most popular on-offer {{ Session::get('education_level')["longName"] }} projects viewed by students.</p>
 
-			<div class="content">
-				<p class="subtitle">Your {{ count($projects) }} most popular on-offer {{ Session::get('education_level')["longName"] }} projects viewed by students.</p>
-
-				<table class="table table-hover bg-white  data-table table--dark-head sort-table">
-					<thead>
-						<tr>
-							<th class="cursor--pointer">Project</th>
-							<th title="A view is counted when a student visits one of your project pages for longer than 5 seconds." class="cursor--pointer"><span class="dashed-underline">View Count</span></th>
+			<table class="table table-hover bg-white data-table sort-table">
+				<thead>
+					<tr>
+						<th class="cursor--pointer">Project</th>
+						<th title="A view is counted when a student visits one of your project pages for longer than 5 seconds." class="cursor--pointer"><span class="dashed-underline">View Count</span></th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($projects as $project)
+						<tr class="project-row" data-title="{{ $project->title }}" data-description="{{ $project->description }}">
+							<td><a href="{{ action('ProjectController@show', $project->id) }}">{{ $project->title }}</a></td>
+							<td>{{ $project->view_count }}</td>
 						</tr>
-					</thead>
-					<tbody>
-						@foreach($projects as $project)
-							<tr class="project-row" data-title="{{ $project->title }}" data-description="{{ $project->description }}">
-								<td><a href="{{ action('ProjectController@show', $project->id) }}">{{ $project->title }}</a></td>
-								<td>{{ $project->view_count }}</td>
-							</tr>
-						@endforeach
-					</tbody>
-				</table>
+					@endforeach
+				</tbody>
+			</table>
 
-				<hr style="margin: 20px 0 ">
+			<hr style="margin: 20px 0 ">
 
-				<h2>Word Cloud</h2>
+			<h2>Word Cloud</h2>
 
-				<div class="flex flex--row flex--stretch-children">
-					<span>The Word Cloud shows the most used words in your projects.</span>
+			<div class="flex flex--row flex--stretch-children">
+				<span>The Word Cloud shows the most used words in your projects.</span>
 
-					<div class="form-field--toggle" style="text-align: right">
-						<p class="switch-label" style="top: 0" >Exclude descriptions</p>
-						<label class="toggle">
-							<input type="checkbox" id="word-cloud-exclude-description" class="boolean-checkbox">
-							<span class="slider"></span>
-						</label>
-					</div>
+				<div class="form-field--toggle" style="text-align: right">
+					<p class="switch-label" style="top: 0" >Exclude descriptions</p>
+					<label class="toggle">
+						<input type="checkbox" id="word-cloud-exclude-description" class="boolean-checkbox">
+						<span class="slider"></span>
+					</label>
 				</div>
-
-				<canvas width="800" height="400" id="word-cloud"></canvas>
-				<canvas width="800" height="400" hidden id="word-cloud-title-only"></canvas>
 			</div>
+
+			<canvas width="800" height="400" id="word-cloud"></canvas>
+			<canvas width="800" height="400" hidden id="word-cloud-title-only"></canvas>
 		</div>
-	@endif
+	</div>
 @endsection
