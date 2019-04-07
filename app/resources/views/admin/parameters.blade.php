14023c859cf543c3fa8015b8106fdb876a2145c6
Lewis Johnson <LewisJohnsonDev@gmail.com>
2019-04-07 16:58:43 +0200

    Added defualt PEQ to parameters view

--- app/resources/views/admin/parameters.blade.php
+++ app/resources/views/admin/parameters.blade.php
@@ -5,6 +5,12 @@
 @endsection
 
 @section ('content')
+
+@php
+	$thresholds = SussexProjects\Mode::getThresholds();
+	$questions = SussexProjects\Mode::getEvaluationQuestions();
+@endphp
+
 <div class="centered mw-800">
 	<div class="card">
 		<div class="card-header">{{ ucfirst(Session::get('education_level')["longName"]) }} Parameters</div>
@@ -43,19 +49,17 @@
 							</select>
 						</div>
 					</div>
+
 					<div class="col-12 col-md-6 border-left">
-						<h5>Thresholds</h5>
+						<h5>Evaluation Thresholds</h5>
 
 						<ul class="list-group" id="thresholds-list">
-							@php
-								$thresholds = SussexProjects\Mode::getThresholds();
-							@endphp
 							@if(count($thresholds) > 0)
-								@foreach(SussexProjects\Mode::getThresholds() as $threshold)
+								@foreach($thresholds as $threshold)
 									<li class="list-group-item">
 										<div class="d-flex">
 											<span>{{ $threshold }}%</span>
-											<button type="button" class="btn btn-sm btn-danger ml-auto js-deleteThreshold">Remove</button>
+											<button type="button" class="btn btn-sm btn-outline-danger ml-auto js-deleteThreshold">Remove</button>
 											<input type="hidden" name="thresholds[]" value="{{ $threshold }}">
 										</div>
 									</li>
@@ -64,8 +68,54 @@
 						</ul>
 
 						<div class="mt-3">
-							<input id="new-threshold-value" class="w-50" name="topic_name" placeholder="Threshold value" type="number" min="0" max="100">
-							<button id="new-threshold-button" class="btn btn-primary ml-2" type="button">Add</button>
+							<input id="new-threshold-value" class="w-50" placeholder="Threshold value" type="number" min="0" max="100">
+							<button id="new-threshold-button" class="btn btn-outline-primary ml-2" type="button">Add</button>
+						</div>
+					</div>
+
+					<div class="col-12 border-top mt-3">
+						<h5 class="mt-3">Evaluation Questions</h5>
+
+						<ul class="list-group" id="questions-list">
+							@if(count($questions) > 0)
+								@foreach($questions as $question)
+									<li class="list-group-item">
+										<div class="row">
+											<div class="col-12 d-flex">
+												Question {{ $loop->iteration }}
+												<button type="button" class="btn btn-sm btn-outline-danger ml-auto js-deleteQuestion">Remove</button>
+											</div>
+											<div class="col-8">
+												<label>Title</label>
+												<input class="form-control" type="text" name="title[]" value="{{ $question->title }}">
+											</div>
+											<div class="col-4">
+												<label>Type</label>
+												<select class="form-control" name="type[]">
+													<option @if($question->type == 3) selected @endif value="3">Poster Presentation</option>
+													<option @if($question->type == 4) selected @endif value="4">Oral Presentation</option>
+													<option @if($question->type == 5) selected @endif value="5">Dissertation</option>
+													<option @if($question->type == 9) selected @endif value="9">Student Feedback</option>
+													<option disabled><hr></option>
+													<option @if($question->type == 0) selected @endif value="0">Plain Text</option>
+													<option @if($question->type == 1) selected @endif value="1">Scale (Fail > Excellent)</option>
+													<option @if($question->type == 2) selected @endif value="2">Number</option>
+													<option @if($question->type == 6) selected @endif value="6">Yes/No</option>
+													<option @if($question->type == 7) selected @endif value="7">Yes/Possibly/No</option>
+													<option @if($question->type == 8) selected @endif value="8">Comment Only</option>
+												</select>
+											</div>
+										</div>
+
+										<label class="mt-2">Description</label>
+										<input class="form-control" type="text" name="description[]	" value="{{ $question->description }}">
+									</li>
+								@endforeach
+							@endif
+						</ul>
+
+						<div class="mt-3">
+							<button id="new-question-button" class="btn btn-outline-primary ml-2" type="button">Add New Question</button>
 						</div>
 					</div>
 				</div>
