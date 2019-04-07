f27941301f2d1f55ebed601660a9b69af3f5b2c7
Lewis Johnson <LewisJohnsonDev@gmail.com>
2019-04-07 17:06:12 +0200

    Moved department selection to header

--- app/resources/views/partials/footer.blade.php
+++ app/resources/views/partials/footer.blade.php
@@ -1,96 +1,29 @@
-<footer class="footer mt-auto py-1 bg-dark w-100 d-print-none">
-	<div class="container-fluid">
-		<div id="levelSelector" class="row">
-			<div class="col-6">
-				<ul class="list-inline mb-0">
-					{{-- DPERARTMENT LIST --}}
-					@foreach(get_departments() as $key => $department)
-						<li class="list-inline-item mr-0">
-							<form role="form" method="POST" action="{{ action('HomeController@setDepartment') }}">
-								{{ csrf_field() }}
-								<input type="hidden" name="department" value="{{ $department }}">
-								<button type="submit" class="btn btn-sm rounded-0 btn-link @if(Session::get('department') == $department) border-bottom text-white @endif" href="#">{{ ucfirst($department) }}</button>
-							</form>
-						</li>
-
-						@if($loop->iteration != count(get_departments()))
-							<span class="mx-2 text-muted d-none d-md-inline-block">|</span>
-						@endif
-					@endforeach
-				</ul>
-			</div>
-
-			<div class="col-6 text-right">
-				{{-- USER EDUCATION LEVEL LIST --}}
-				@if(Auth::check())
-					<ul class="list-inline mb-0">
-						@foreach(Auth::user()->allowedEducationLevel() as $key => $level)
-							<li class="list-inline-item">
-								<a href="?educationLevel={{ $level['shortName'] }}" class="btn btn-sm rounded-0 btn-link @if(Session::get('education_level') == $level) border-bottom text-white @endif">{{ ucfirst($level["longName"]) }}</a>
-							</li>
-
-							@if($loop->iteration != count(Auth::user()->allowedEducationLevel()))
-								<span class="mx-2 text-muted d-none d-md-inline-block">|</span>
-							@endif
-						@endforeach
-					</ul>
-				@endif
-			
-				{{-- GUEST EDUCATION LEVEL LIST --}}
-				@if(ldap_guest())
-					<ul class="list-inline mb-0">
-						@foreach(SussexProjects\User::guestEducationLevel() as $key => $level)
-							<li class="list-inline-item">
-								<a href="?educationLevel={{ $level['shortName'] }}" class="btn btn-sm rounded-0 btn-link @if(Session::get('education_level') == $level) border-bottom text-white @endif" >{{ ucfirst($level["longName"]) }}</a>
-							</li>
-
-							@if($loop->iteration != count(SussexProjects\User::guestEducationLevel()))
-								<span class="mx-2 text-muted d-none d-md-inline-block">|</span>
-							@endif
-						@endforeach
-					</ul>
-				@endif
-			</div>
-		</div>
-
-		<div class="row">
-			<div class="col-4">
-				@if(get_config_json("footer.accessibility_buttons.value") == true)
-					@if(Cookie::get('accessibility_font') == "true")
-						<a class="btn btn-sm rounded-0 btn-link border-bottom text-white text-left" href="?large_font=false">Standard Font</a>
-					@else
-						<a class="btn btn-sm rounded-0 btn-link text-left" href="?large_font=true">Large Font</a>
-					@endif
-					
-					<span class="mx-2 text-muted d-none d-md-inline-block">|</span>
-
-					@if(Cookie::get('accessibility_contrast') == "true")
-						<a class="btn btn-sm rounded-0 btn-link border-bottom text-white text-left" href="?high_contrast=false">High Contrast</a>
-					@else
-						<a class="btn btn-sm rounded-0 btn-link text-left" href="?high_contrast=true">High Contrast</a>
-					@endif
-				@endif
-
-				@if(ldap_guest())
-					<p>Guest Mode</p>
-				@endif
-			</div>
+<footer class="py-1 row d-print-none mt-5" style="background-color: rgba(0, 0, 0, 0.03)">
+	<div class="col-4">
+		@if(Cookie::get('accessibility_font') == "true")
+			<a href="?large_font=false">Standard Font</a>
+		@else
+			<a href="?large_font=true">Large Font</a>
+		@endif
 		
-			<div class="col-8 text-right">
-				<a id="leave-feedback-button" href="#" data-is-guest="{{ Auth::guest() ? 'true' : 'false' }}">Leave Feedback</a>
+		<span class="mx-2 text-muted d-none d-md-inline-block">|</span>
 
-				<span class="mx-2 text-muted d-none d-md-inline-block">|</span>
+		@if(Cookie::get('accessibility_contrast') == "true")
+			<a href="?high_contrast=false">Standard Contrast</a>
+		@else
+			<a href="?high_contrast=true">High Contrast</a>
+		@endif
 
-				<a target="_blank" href="@lang("messages.footer_link_url")">@lang("messages.footer_link_text")</a>
+		@if(ldap_guest())
+			<p>Guest Mode</p>
+		@endif
+	</div>
 
-				<span class="mx-2 text-muted d-none d-md-inline-block">|</span>
+	<div class="col-8 text-right">
+		<a id="leave-feedback-button" href="#" data-is-guest="{{ Auth::guest() ? 'true' : 'false' }}">Leave Feedback</a>
 
-				@if(Session::get('education_level') != null)
-					<span class="text-muted">{{ lang_sess("footer_maintainer_text") }}</span>
-				@else
-					<span class="text-muted">@lang("messages.footer_maintainer_text")</span>
-				@endif
-			</div>
-		</div>
+		<span class="mx-2 text-muted d-none d-md-inline-block">|</span>
+
+		<a target="_blank" href="@lang("messages.footer_link_url")">@lang("messages.footer_link_text")</a>
 	</div>
 </footer>
