39c08b75f4ced6fe7231242a250bcf2eace4732f
Lewis Johnson <lewisjohnsondev@gmail.com>
2019-04-26 16:09:57 +0200

    Added col checks

--- app/database/migrations/2019_02_09_162734_add_marker_id_to_project_table.php
+++ app/database/migrations/2019_02_09_162734_add_marker_id_to_project_table.php
@@ -15,11 +15,13 @@ class AddMarkerIdToProjectTable extends Migration {
 			foreach(get_education_levels() as $key => $level) {
 				$projectTableName = $department.'_projects_'.$level['shortName'];
 
-				// Add the marker id to the project table
-				Schema::table($projectTableName, function (Blueprint $table) use ($department, $level){
-					$table->uuid('marker_id')->nullable(true);
-					$table->foreign('marker_id')->references('id')->on($department.'_supervisors')->onDelete('SET NULL');
-				});
+				if(!Schema::hasColumn($projectTableName, 'marker_id')){
+					// Add the marker id to the project table
+					Schema::table($projectTableName, function (Blueprint $table) use ($department, $level){
+						$table->uuid('marker_id')->nullable(true);
+						$table->foreign('marker_id')->references('id')->on($department.'_supervisors')->onDelete('SET NULL');
+					});
+				}
 			}
 		}
 	}
--- app/database/migrations/2019_02_09_162736_remove_marker_id_from_student_table.php
+++ app/database/migrations/2019_02_09_162736_remove_marker_id_from_student_table.php
@@ -14,10 +14,13 @@ class RemoveMarkerIdFromStudentTable extends Migration {
 		foreach(get_departments() as $key => $department) {
 			foreach(get_education_levels() as $key => $level) {
 				$studentTableName = $department.'_students_'.$level['shortName'];
-				// Remove the marker id from the student table
-				Schema::table($studentTableName, function (Blueprint $table) use ($department, $level){
-					$table->dropColumn('marker_id');
-				});
+
+				if(Schema::hasColumn($studentTableName, 'marker_id')){
+					// Remove the marker id from the student table
+					Schema::table($studentTableName, function (Blueprint $table) use ($department, $level){
+						$table->dropColumn('marker_id');
+					});
+				}
 			}
 		}
 	}
