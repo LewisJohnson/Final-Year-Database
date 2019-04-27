479e461a56dff71132592711b6ec6052a1e25b36
Lewis Johnson <lewisjohnsondev@gmail.com>
2019-04-27 17:32:09 +0200

    Cleaned dashboard a little

--- app/resources/views/admin/system/dashboard.blade.php
+++ app/resources/views/admin/system/dashboard.blade.php
@@ -22,53 +22,41 @@
 				<div class="content" aria-expanded="false" aria-hidden="true">
 					<h2>System</h2>
 					<div class="dashboard-section">
-						<h3>Environment</h3>
-						<p>To configure environment variables, edit the <b>.env</b> file in the <b>root</b> directory. A server restart is required for changes to take effect.</p>
-						<div class="config-danger">
-							<p class="text-icon">&#9888;&#65039;</p>
-							<p><b>Be careful!</b> You might break something making the website unusable.</p>
-						</div>
+						<h5>Environment</h5>
+						<p>
+							To configure environment variables, edit the <b>.env</b> file in the <b>root</b> directory.
+							A server restart is required for changes to take effect.
+						</p>
 					</div>
 
-					<div class="dashboard-section">
-						<h3>Laravel</h3>
-						<p>To configure Lavavel (Server) variables, edit any <b>config\*.php</b> file. A server restart <em>might be</em> required for changes to take effect.</p>
-					</div>
+					
+					<h5 class="mt-3">Laravel</h5>
+					<p>
+						To configure Lavavel (Server) variables, edit any <code>config\*.php</code> file.
+						A server restart <em>might be</em> required for changes to take effect.
+					</p>
+					
+					<h5 class="mt-3">System</h5>
+					<p>
+						To manually configure system variables, edit the <code>storage\app\config\config.json</code>, otherwise use the system dashboard.
+						Changes will take effect immediately.
+					</p>
+					
+					<h5 class="mt-3">Style</h5>
+					<p>
+						To configure style variables, edit <code>resources\assets\sass\_variables.scss</code>.
+						The SCSS\SASS bundle will have to be recompiled for changes to take effect.
+						To do this, run the following command;
+					</p>
+					<code>npm run prod</code>
+								
+					<h5 class="mt-3">JavaScript</h5>
+					<p>To configure JavaScript variables, edit <code>resources\assets\config.js</code>.
+						You will have to re-bundle the JavaScript files with Webpack for changes to take effect.
+						To do this, run the following command;
+					</p>
 
-					<div class="dashboard-section">
-						<h3>System</h3>
-						<p>To manually configure system variables, edit the <b>storage\app\config\config.json</b>, otherwise use the system dashboard. Changes will take effect immediately.</p>
-					</div>
-
-					<div class="dashboard-section">
-						<h3>Style</h3>
-						<p>To configure style variables, edit <b>resources\assets\sass\_variables.scss</b>.
-							The SCSS\SASS bundle will have to be recompiled for changes to take effect.
-							To do this, run this command in the <b>root</b> directory.</p>
-						<code>npm run production</code>
-					</div>
-
-					<div class="dashboard-section">
-						<h3>JavaScript</h3>
-						<p>To configure JavaScript variables, edit <b>resources\assets\config.js</b>.
-							You will have to re-bundle the JavaScript files with Webpack for changes to take effect.
-							To do this, run this command in the <b>root</b> directory.</p>
-
-						<code>npm run production</code>
-
-						<h4>Hotfix</h4>
-						<p>If you want to apply a hotfix (Apply changes without having to re-bundle JavaScript), you may edit the <b>\public\js\config.js</b> file.</p>
-
-						<div class="config-tip">
-							<p class="text-icon">&#128161;</p>
-							<p>Hotfix changes will be overwritten when Webpack is re-bundled.</p>
-						</div>
-
-						<div class="config-danger">
-							<p class="text-icon">&#9888;&#65039;</p>
-							<p><b>Be careful!</b> You might break something making the website unusable.</p>
-						</div>
-					</div>
+					<code>npm run prod</code>
 				</div>
 			</li>
 
@@ -77,10 +65,10 @@
 				<div class="content" aria-expanded="false" aria-hidden="true">
 					<h2>Departments</h2>
 
-					<h3>Add New Department</h3>
-					<p>To add a new department, edit the <tt>app\config\system.json</tt> file.</p>
+					<h5>New Department</h5>
+					<p>To add a new department, contact Lewis Johnson.</p>
 
-					<h3>Existing</h3>
+					<h5>Existing Departments</h5>
 					<ul>
 						@foreach(get_departments() as $dep)
 							<li>
@@ -90,6 +78,7 @@
 					</ul>
 				</div>
 			</li>
+
 			<li class="tab" data-tab-name="User Agent">
 				<button class="btn w-100 text-left js-tab-button">User Agent</button>
 				<div class="content" aria-expanded="false" aria-hidden="true">
@@ -97,7 +86,7 @@
 					<form class="form d-flex flex-column" role="form" method="POST" action="{{ action('SystemAdminController@updateSystemConfiguration') }}">
 						{{ csrf_field() }}
 
-						<div class="form-field form-field--flex form-field--toggle">
+						<div class="form-group form-group--flex form-group--toggle">
 							<p class="switch-label" for="user_agent">Collect user agent strings</p>
 							<label class="toggle">
 								<input type="checkbox" name="collect_user_agent" id="collect_user_agent"class="boolean-checkbox" @if(get_config_json('user_agent.collect_user_agent.value')) checked @endif>
@@ -107,7 +96,7 @@
 						<label class="description">{{ get_config_json("user_agent.collect_user_agent.description") }}</label>
 						<input type="hidden" name="collect_user_agent-json" value="user_agent.collect_user_agent">
 
-						<div class="form-field form-field--flex form-field--toggle">
+						<div class="form-group form-group--flex form-group--toggle">
 							<p class="switch-label" for="collect_referrer">Collect referrer url</p>
 							<label class="toggle">
 								<input type="checkbox" name="collect_referrer" id="collect_referrer" class="boolean-checkbox" @if(get_config_json('user_agent.collect_referrer.value')) checked @endif>
@@ -117,7 +106,7 @@
 						<label class="description">{{ get_config_json("user_agent.collect_referrer.description") }}</label>
 						<input type="hidden" name="collect_referrer-json" value="user_agent.collect_referrer">
 
-						<div class="form-field form-field--flex">
+						<div class="form-group form-group--flex">
 							<button class="btn w-100 text-left button--raised button--accent" type="submit">Save</button>
 						</div>
 					</form>
@@ -130,29 +119,29 @@
 					<h2>Header</h2>
 					<form class="form d-flex flex-column" role="form" method="POST" action="{{ action('SystemAdminController@updateSystemConfiguration') }}">
 						{{ csrf_field() }}
-
-						<div class="form-field">
-							<label for="header_logo_url">Logo URL</label>
-							<label class="description">{{ get_config_json("header.logo_url.description") }}</label>
-							<input type="text" name="header_logo_url" id="header_logo_url" value="{{ get_config_json("header.logo_url.value") }}">
+						<h5>Logo</h5>
+						<div class="form-group">
+							<p class="text-muted">{{ get_config_json("header.logo_url.description") }}</p>
+							<input class="form-control" type="text" name="header_logo_url" id="header_logo_url" value="{{ get_config_json("header.logo_url.value") }}">
 						</div>
 						<input type="hidden" name="header_logo_url-json" value="header.logo_url">
 
-						<div class="form-field">
+						<h5>Accent Colour</h5>
+						<div class="form-group">
 							<label for="header_background">Background</label>
-							<label class="description">{{ get_config_json("header.background.description") }}</label>
-							<label class="description"> Accepted types:
+							<label class="text-muted">{{ get_config_json("header.background.description") }}</label>
+							<label class="text-muted"> Accepted types:
 								@foreach(get_config_json("header.background.type") as $type)
 									<span>{{ $type }}@if(!$loop->last), @endif</span>
 								@endforeach
 							</label>
-							<input type="text" name="header_background" id="header_background" value="{{ get_config_json("header.background.value") }}">
+							<input class="form-control" type="text" name="header_background" id="header_background" value="{{ get_config_json("header.background.value") }}">
 						</div>
 
 						<input type="hidden" name="header_background-json" value="header.background">
 
-						<div class="form-field form-field--flex">
-							<button class="btn w-100 text-left button--raised button--accent" type="submit">Save</button>
+						<div class="text-right">
+							<button class="btn btn-primary" type="submit">Save</button>
 						</div>
 					</form>
 				</div>
