64f82d3ea577809915947791af166cbec0ea4002
Lewis Johnson <LewisJohnsonDev@gmail.com>
2019-08-06 21:48:07 +0100

    Update ProjectEvaluationController.php

--- app/app/Http/Controllers/ProjectEvaluationController.php
+++ app/app/Http/Controllers/ProjectEvaluationController.php
@@ -30,16 +30,6 @@ use SussexProjects\PEQValueTypes;
 class ProjectEvaluationController extends Controller {
 
 	public function __construct(){
-		$this->middleware(function ($request, $next) {
-			if(!(Auth::user()->isExternalMarker() || Auth::user()->isAdminOfEducationLevel(Session::get('education_level')["shortName"]))) {
-				if(Mode::getProjectEvaluationDate()->gte(\Carbon\Carbon::now())) {
-					return abort(403);
-				}
-			}
-			
-			return $next($request);
-		});
-
 		parent::__construct();
 	}
 
