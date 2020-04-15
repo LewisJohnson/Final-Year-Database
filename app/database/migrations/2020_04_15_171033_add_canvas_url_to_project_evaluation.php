3b67f229c6589d76c15ba51c7fa667322aaf9f04
Lewis Johnson <LewisJohnsonDev@gmail.com>
2020-04-15 21:14:28 +0100

    Added project evaluation canvas url migration

---
+++ app/database/migrations/2020_04_15_171033_add_canvas_url_to_project_evaluation.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCanvasUrlToProjectEvaluation extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		foreach(get_departments() as $key => $department) {
+			foreach(get_education_levels() as $key => $level) {
+				Schema::table($department.'_project_evaluation_'.$level['shortName'], function (Blueprint $table) use ($department, $level){
+					$table->string('canvas_url')->default(0);
+				});
+			}
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		 foreach(get_departments() as $key => $department) {
+			foreach(get_education_levels() as $key => $level) {
+				Schema::table($department.'_project_evaluation_'.$level['shortName'], function (Blueprint $table) use ($department, $level){
+					$table->dropColumn("canvas_url");
+				});
+			}
+		}
+	}
+}
