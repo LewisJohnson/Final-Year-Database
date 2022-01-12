7fecae5d9e90c521d03b3922907b03e9061dc7c9
Lewis Johnson <LewisJohnsonDev@gmail.com>
2022-01-12 21:12:42 +0000

    Debug banner is no longer shown on print

--- app/resources/views/partials/debug-banner.blade.php
+++ app/resources/views/partials/debug-banner.blade.php
@@ -1,4 +1,4 @@
-<div class="flex bg-warning">
+<div class="flex bg-warning d-print-none">
 	<p class="text-center py-5 m-0">
 		<b>App running in debug mode</b>
 		<br>Don't worry, only you can see this message. Please disable debugging mode in the <b>.env</b> file</p>
