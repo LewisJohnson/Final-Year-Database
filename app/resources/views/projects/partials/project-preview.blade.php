bb597dc7aa10321256d06d05241804f89ac0b094
Lewis Johnson <LewisJohnsonDev@gmail.com>
2022-01-12 21:12:59 +0000

    Project preview is now clearer to read with black text

--- app/resources/views/projects/partials/project-preview.blade.php
+++ app/resources/views/projects/partials/project-preview.blade.php
@@ -1,15 +1,15 @@
-<div class="project-preview">
+<div class="project-preview text-left text-dark">
 	<div class="project">
-		<h1 class="title">{{ $project->title }}</h1>
-		<h2 class="supervisor">{{ $project->supervisor->user->getFullName() }}</h2>
+		<h2 class="title">{{ $project->title }}</h2>
+		<h3 class="supervisor">{{ $project->supervisor->user->getFullName() }}</h3>
 
-		<h3>Description</h3>
+		<h5>Description</h5>
 		<p>{!! html_entity_decode($project->description) !!}</p>
 
-		<h3>Skills</h3>
+		<h5>Skills</h5>
 		<p>{{ $project->skills }}</p>
 
-		<h3>Topics</h3>
+		<h5>Topics</h5>
 		<ul class="topics-list">
 			@if(count($project->topics))
 				@foreach($project->topics as $topic)
