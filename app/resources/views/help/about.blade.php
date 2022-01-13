10cc082fad0a7f6d28d60e35c4e1bf94ef2ac461
Lewis Johnson <LewisJohnsonDev@gmail.com>
2022-01-13 00:11:41 +0000

    Updated about page style to match others

--- app/resources/views/help/about.blade.php
+++ app/resources/views/help/about.blade.php
@@ -1,36 +1,33 @@
 @extends('layouts.app')
 @section ('content')
-	<div class="centered mw-800">
-		<div class="card">
-			<h5 class="card-header">About</h5>
-			<div class="card-body">
-				<p>@lang("help.about")</p>
+	<div class="centered mw-800 bg-white shadow-sm rounded p-4">
+		<h2 class="main-title">About</h2>
+		
+		<p>@lang("help.about")</p>
 
-				<div class="row">
-					<div class="col-3">
-						<a href="https://github.com/lewisJohnson/Final-Year-Database">
-							<img src="{{ asset('images/github-logo.png') }}" alt="The github logo" class="img-thumbnail">
-						</a>
-					</div>
-		
-					<div class="col-3">
-						<a href="https://laravel.com" title="Laravel">
-							<img src="{{ asset('images/laravel-logo.png') }}" alt="The Laravel logo" class="img-thumbnail">
-						</a>
-					</div>
-		
-					<div class="col-3">
-						<a href="https://nodejs.org" title="NodeJs">
-							<img src="{{ asset('images/nodejs-logo.png') }}" alt="The NodeJs logo" class="img-thumbnail">
-						</a>
-					</div>
-		
-					<div class="col-3">
-						<a href="https://getbootstrap.com" title="Bootstrap">
-							<img src="{{ asset('images/bootstrap-logo.png') }}" alt="The Bootstrap logo" class="img-thumbnail">
-						</a>
-					</div>
-				</div>
+		<div class="row">
+			<div class="col-3">
+				<a href="https://github.com/lewisJohnson/Final-Year-Database">
+					<img src="{{ asset('images/github-logo.png') }}" alt="The github logo" class="img-thumbnail">
+				</a>
+			</div>
+
+			<div class="col-3">
+				<a href="https://laravel.com" title="Laravel">
+					<img src="{{ asset('images/laravel-logo.png') }}" alt="The Laravel logo" class="img-thumbnail">
+				</a>
+			</div>
+
+			<div class="col-3">
+				<a href="https://nodejs.org" title="NodeJs">
+					<img src="{{ asset('images/nodejs-logo.png') }}" alt="The NodeJs logo" class="img-thumbnail">
+				</a>
+			</div>
+
+			<div class="col-3">
+				<a href="https://getbootstrap.com" title="Bootstrap">
+					<img src="{{ asset('images/bootstrap-logo.png') }}" alt="The Bootstrap logo" class="img-thumbnail">
+				</a>
 			</div>
 		</div>
 	</div>
