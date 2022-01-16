c9bf488fe7b1c88f910a715c47fdf92f21506d3f
Lewis Johnson <LewisJohnsonDev@gmail.com>
2022-01-16 19:17:43 +0000

    Updated transactions page style to look like other admin pages
    - Added tooltip to checkbox

--- app/resources/views/admin/transactions.blade.php
+++ app/resources/views/admin/transactions.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
 @section('content')
-<div class="centered mw-1400">
-	<h1>{{ $transaction_type }} Transactions</h1>
+@php
+	$showTypeColumn = $transaction_type == 'All' ? 'true' : 'false';
+@endphp
+
+<div class="centered mw-1400 bg-white shadow-sm rounded p-4">
+	<h2 class="main-title">Transactions <small class="text-muted">/ {{ $transaction_type }}</small></h2>
+
 
 	<div class="d-flex">
 		<div class="btn-group">
@@ -25,79 +30,84 @@
 				href="{{ action('TransactionController@index', 'type=topic') }}"
 				data-element-to-replace-with-loader-selector="#responsive-table">Topic</a>
 		</div>
-		<div class="checkbox ml-auto">
+		<div class="checkbox ml-auto" title="Shows the database value for most columns on hover">
 			<input type="checkbox" id="showTransactionDetailOnHover">
 			<label class="ml-1" for="showTransactionDetailOnHover">Enable detail on hover</label>
 		</div>
 	</div>
 
-
 	<div class="table-responsive">
-		<table id="transaction-table" class="table table-sm bg-white data-table table-column-toggle sort-table shadow-sm mt-3">
+		<table id="transaction-table"
+			class="table table-sm bg-white data-table table-column-toggle sort-table shadow-sm mt-3">
 			<thead>
 				<tr>
 					<th data-default="false" class="cursor--pointer">Id</th>
+					<th data-default="{{ $showTypeColumn }}" class="cursor--pointer">Type</th>
 					<th data-default="true" class="cursor--pointer">Action</th>
-					<th data-default="{{ $transaction_type == "Topic" }}" class="cursor--pointer">Topic</th>
-					<th data-default="{{ $transaction_type != "Topic" }}" class="cursor--pointer">Project</th>
-					<th data-default="{{ $transaction_type != "Topic" }}" class="cursor--pointer">Student</th>
+					<th data-default="{{ $transaction_type == 'Topic' }}" class="cursor--pointer">Topic</th>
+					<th data-default="{{ $transaction_type != 'Topic' }}" class="cursor--pointer">Project</th>
+					<th data-default="{{ $transaction_type != 'Topic' }}" class="cursor--pointer">Student</th>
 					<th data-default="true" class="cursor--pointer">Supervisor</th>
 					<th data-default="false" class="cursor--pointer">Marker</th>
-					<th data-default="{{ $transaction_type == "Topic" }}" class="cursor--pointer">Admin</th>
+					<th data-default="{{ $transaction_type == 'Topic' }}" class="cursor--pointer">Admin</th>
 					<th data-default="true" class="cursor--pointer">Date</th>
 				</tr>
 			</thead>
 			<tbody>
-				@foreach($transactions as $transaction)
-					<tr>
-						<td data-hover="{{ $transaction->id }}">{{ substr($transaction->id, 0, 7) }}</td>
-						<td>{{ ucfirst($transaction->action) }}</td>
+				@foreach ($transactions as $transaction)
+				<tr>
+					<td data-hover="{{ $transaction->id }}">{{ substr($transaction->id, 0, 7) }}</td>
+					<td>{{ ucfirst($transaction->type) }}</td>
+					<td>{{ ucfirst($transaction->action) }}</td>
 
-						<td>{{ $transaction->topic }}</td>
+					<td>{{ $transaction->topic }}</td>
 
-						@if($transaction->action == "copy")
-							@php
-								$splitProj = explode('->', $transaction->project);
-								$originalProject = \SussexProjects\Project::find(trim($splitProj[0]));
-								$copiedProject =  \SussexProjects\Project::find(trim($splitProj[1]));
-							@endphp
+					{{-- Project column --}}
+					@if ($transaction->action == 'copy')
+						@php
+							$splitProj = explode('->', $transaction->project);
+							$originalProject = \SussexProjects\Project::find(trim($splitProj[0]));
+							$copiedProject = \SussexProjects\Project::find(trim($splitProj[1]));
+						@endphp
 
 							<td data-hover="{{ $transaction->project }}">
-								<a href="{{ action('ProjectController@show', $originalProject) }}">{{ $originalProject->title }}</a>
+								<a href="{{ action('ProjectController@show', $originalProject) }}">{{ $originalProject->title}}</a>
 								<span>⮕</span>
 								<a href="{{ action('ProjectController@show', $copiedProject) }}">{{ $copiedProject->title }}</a>
 							</td>
-						@elseif($transaction->action == "deleted")
-							<td>
-								{{ $transaction->project }}
-							</td>
-						@elseif($transaction->action == "undo")
-							<td>
-								{{ $transaction->project }}
+					@elseif($transaction->action == 'deleted' || $transaction->action == 'undo')
+						<td>
+							{{ $transaction->project }}
+						</td>
+					@else
+						@php
+							$projTitle = $transaction->getProjectTitle();
+						@endphp
+
+						@if ($projTitle != '-')
+							<td data-hover="{{ $transaction->project }}"><a
+									href="{{ action('ProjectController@show', $transaction->project) }}">{{ $projTitle }}</a>
 							</td>
 						@else
-							@php
-								$projTitle = $transaction->getProjectTitle();
-							@endphp
-
-							@if($projTitle != '-')
-								<td data-hover="{{ $transaction->project }}"><a href="{{ action('ProjectController@show', $transaction->project) }}">{{ $projTitle }}</a></td>
-							@else
-								<td data-hover="{{ $transaction->project }}">{{ $projTitle }}</td>
-							@endif
+							<td data-hover="{{ $transaction->project }}">{{ $projTitle }}</td>
 						@endif
+					@endif
 
-						<td data-hover="{{ $transaction->student }}">{{ $transaction->getName($transaction->student) }}</td>
-						<td data-hover="{{ $transaction->supervisor }}">{{ $transaction->getName($transaction->supervisor) }}</td>
-						<td data-hover="{{ $transaction->marker }}"	>{{  $transaction->getName($transaction->marker) }}</td>
-						<td data-hover="{{ $transaction->admin }}">{{ $transaction->getName($transaction->admin) }}</td>
-						<td data-use-hover-value data-hover="{{ $transaction->transaction_date }}">{{ $transaction->transaction_date->format('d/m/yy H:i:s') }}</td>
-					</tr>
+					{{-- Student column --}}
+					<td data-hover="{{ $transaction->student }}">{{ $transaction->getName($transaction->student) }}</td>
+							
+					{{-- Supervisor column --}}
+					<td data-hover="{{ $transaction->supervisor }}">{{ $transaction->getName($transaction->supervisor) }}</td>
+
+
+					<td data-hover="{{ $transaction->admin }}">{{ $transaction->getName($transaction->admin) }}</td>
+					<td data-use-hover-value data-hover="{{ $transaction->transaction_date }}">{{ $transaction->transaction_date->format('d/m/yy H:i:s') }}</td>
+				</tr>
 				@endforeach
 			</tbody>
 		</table>
 	</div>
-		
+
 	{{ $transactions->links() }}
 </div>
 @endsection
