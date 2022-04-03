45c2deab54c5546bed20c1e395ddcdecb5a7c7f6
Lewis Johnson <LewisJohnsonDev@gmail.com>
2022-04-03 20:23:40 +0100

    Formatted supervisor.js

--- app/resources/assets/js/views/supervisor.js
+++ app/resources/assets/js/views/supervisor.js
@@ -7,7 +7,7 @@
 import Swappable from '@shopify/draggable/lib/swappable';
 
 
-;$(function() {
+; $(function () {
 	"use strict";
 
 	/* =================================
@@ -19,7 +19,7 @@ import Swappable from '@shopify/draggable/lib/swappable';
 	*
 	* @param {HTMLElement} element The element that will be upgraded.
 	*/
-	var ProjectTopics = function ProjectTopics() {};
+	var ProjectTopics = function ProjectTopics() { };
 	window["ProjectTopics"] = ProjectTopics;
 
 	ProjectTopics.prototype.CssClasses_ = {
@@ -30,11 +30,11 @@ import Swappable from '@shopify/draggable/lib/swappable';
 	ProjectTopics.prototype.Selectors_ = {
 		EDIT_ADD_TOPIC_INPUT: '#add-topic-input',
 		EDIT_NEW_TOPIC_INPUT_CONTAINER: '#new-topic-input-container',
-		EDIT_PROJECT_FORM : '#edit-project-form',
-		
+		EDIT_PROJECT_FORM: '#edit-project-form',
+
 		CREATE_ADD_TOPIC_INPUT: '#create-project-add-topic-input',
 		CREATE_NEW_TOPIC_INPUT_CONTAINER: '#create-topic-input-container',
-		CREATE_PROJECT_FORM : '#create-project-form',
+		CREATE_PROJECT_FORM: '#create-project-form',
 	};
 
 	var projectTopics = new ProjectTopics();
@@ -42,10 +42,10 @@ import Swappable from '@shopify/draggable/lib/swappable';
 	ProjectTopics.prototype.functions = {
 		addTopicToProjectBeingCreated: function (topicName) {
 			let isDuplicate = false;
-			
+
 			if ($(".topics-list.create li.topic:last").length > 0) {
 
-				$(".topics-list.create li.topic .topic-name").each(function() {
+				$(".topics-list.create li.topic .topic-name").each(function () {
 					if ($(this).val() == topicName) {
 						isDuplicate = true;
 					}
@@ -53,35 +53,35 @@ import Swappable from '@shopify/draggable/lib/swappable';
 
 				if (!isDuplicate) {
 					$(".topics-list.create li.topic:last").after(`
-						<li class="topic" data-topic-id="${ topicName }">
+						<li class="topic" data-topic-id="${topicName}">
 							<button type="button" class="btn rounded-0 topic-remove">X</button>
-							<input type="text" readonly name="topics[]" class="topic-name" size="${ topicName.length }" value="${ topicName }">
+							<input type="text" readonly name="topics[]" class="topic-name" size="${topicName.length}" value="${topicName}">
 						</li>`);
 				}
 			} else {
 				$(".topics-list.create").prepend(`
-					<li class="topic first" data-topic-id="${ topicName }">
+					<li class="topic first" data-topic-id="${topicName}">
 						<button type="button" class="btn rounded-0 topic-remove">X</button>
-						<input type="text" readonly name="topics[]" class="topic-name" size="${ topicName.length }" value="${ topicName }">
+						<input type="text" readonly name="topics[]" class="topic-name" size="${topicName.length}" value="${topicName}">
 					</li>`);
 			}
 
-			setTimeout(function(){
+			setTimeout(function () {
 				$(projectTopics.Selectors_.CREATE_ADD_TOPIC_INPUT).val('');
 			}, 20);
 		},
 
 		removeTopicToProjectBeingCreated: function (topicId) {
-			$(".topics-list.create li.topic").each(function(i, obj) {
-				if($(this).data("topic-id") == topicId){
+			$(".topics-list.create li.topic").each(function (i, obj) {
+				if ($(this).data("topic-id") == topicId) {
 					$(this).remove();
 				}
 			});
 		},
 
 		updateProjectPrimaryTopicToProjectBeingCreated: function (topicId) {
-			$(".topics-list.create li.topic").each(function(i, obj) {
-				if($(this).data("topic-id") == topicId){
+			$(".topics-list.create li.topic").each(function (i, obj) {
+				if ($(this).data("topic-id") == topicId) {
 					$(this).addClass("first");
 				} else {
 					$(this).removeClass("first");
@@ -100,11 +100,11 @@ import Swappable from '@shopify/draggable/lib/swappable';
 					topic_name: topicName,
 					project_id: projectId
 				},
-				success: function(response){
+				success: function (response) {
 					$(projectTopics.Selectors_.EDIT_ADD_TOPIC_INPUT).val('');
 
-					if(response.successful){
-						if($(".topics-list.edit li.topic:last").length > 0){
+					if (response.successful) {
+						if ($(".topics-list.edit li.topic:last").length > 0) {
 							$(".topics-list.edit li.topic:last").after('<li class="topic" data-topic-id="' + response.topic.id + '"><button type="button" class="btn rounded-0 topic-remove">X</button><p class="topic-name">' + response.topic.name + '</p></li>');
 						} else {
 							$(".topics-list.edit").prepend('<li class="topic first" data-topic-id="' + response.topic.id + '"><button type="button" class="btn rounded-0 topic-remove">X</button><p class="topic-name">' + response.topic.name + '</p></li>');
@@ -113,7 +113,7 @@ import Swappable from '@shopify/draggable/lib/swappable';
 						createToast('error', response.message);
 					}
 				}
-			}).always(function(data){
+			}).always(function (data) {
 				$('.spinner').hide();
 			});
 		},
@@ -130,16 +130,16 @@ import Swappable from '@shopify/draggable/lib/swappable';
 				type: "PATCH",
 				url: ajaxUrl,
 				data: {
-					topic_id : topicId,
+					topic_id: topicId,
 					project_id: projectId
 				},
-				success: function() {
+				success: function () {
 					$('.topics-list.edit li.topic[data-topic-id="' + topicId + '"]').remove();
 				},
-				error: function() {
+				error: function () {
 					self.show();
 				}
-			}).always(function(){
+			}).always(function () {
 				$(".spinner").hide();
 			});
 		},
@@ -151,19 +151,19 @@ import Swappable from '@shopify/draggable/lib/swappable';
 				type: "PATCH",
 				url: ajaxUrl,
 				data: {
-					topic_id : topicId,
+					topic_id: topicId,
 					project_id: projectId
 				},
-				success: function(){
-					$(".topics-list.edit li.topic").each(function(i, obj) {
-						if($(this).data("topic-id") == topicId){
+				success: function () {
+					$(".topics-list.edit li.topic").each(function (i, obj) {
+						if ($(this).data("topic-id") == topicId) {
 							$(this).addClass("first");
 						} else {
 							$(this).removeClass("first");
 						}
 					});
 				},
-			}).always(function(){
+			}).always(function () {
 				$(".spinner").hide();
 			});
 		}
@@ -178,36 +178,35 @@ import Swappable from '@shopify/draggable/lib/swappable';
 		draggable: ".topic",
 	});
 
-	createSwappable.on('swappable:swapped', function(){
+	createSwappable.on('swappable:swapped', function () {
 		var originalPrimaryTopicId = $(projectTopics.Selectors_.CREATE_PROJECT_FORM).data('primary-topic-id');
 		var topicId = $(".topics-list.create li:first-child").data('topic-id');
 
-		if(topicId != originalPrimaryTopicId){
+		if (topicId != originalPrimaryTopicId) {
 			projectTopics.functions.updateProjectPrimaryTopicToProjectBeingCreated(topicId);
 		}
 	});
 
 	// Add new topic on COMMA pressed
-	$(projectTopics.Selectors_.CREATE_ADD_TOPIC_INPUT).keypress(function(e) {
-		if((e.which == 44 || e.keyCode == 188) && $(this).val() != "") {
+	$(projectTopics.Selectors_.CREATE_ADD_TOPIC_INPUT).keypress(function (e) {
+		if ((e.which == 44 || e.keyCode == 188) && $(this).val() != "") {
 			projectTopics.functions.addTopicToProjectBeingCreated($(this).val());
 		}
 	});
 
 	// Add new topic on input blur
-	$(projectTopics.Selectors_.CREATE_ADD_TOPIC_INPUT).on('blur', function(e) {
-		if($(this).val() != "") {
+	$(projectTopics.Selectors_.CREATE_ADD_TOPIC_INPUT).on('blur', function (e) {
+		if ($(this).val() != "") {
 			projectTopics.functions.addTopicToProjectBeingCreated($(this).val());
 		}
 	});
 
 	// Remove topic
-	$('.topics-list.create').on('click', '.topic .topic-remove', function(){
+	$('.topics-list.create').on('click', '.topic .topic-remove', function () {
 		var topicId = $(this).parent('li').data('topic-id');
 		projectTopics.functions.removeTopicToProjectBeingCreated(topicId);
 	});
 
-	$(projectTopics.Selectors_.CREATE_NEW_TOPIC_INPUT_CONTAINER).on('click', function() {
 		$(projectTopics.Selectors_.CREATE_ADD_TOPIC_INPUT).focus();
 	});
 
@@ -227,50 +226,57 @@ import Swappable from '@shopify/draggable/lib/swappable';
 		projectTopics.functions.updateProjectPrimaryTopic(projectId, topicId);
 	});
 
+	editSwappable.on('swappable:stop', function () {
+		var projectId = $(projectTopics.Selectors_.EDIT_PROJECT_FORM).data('project-id');
+		var topicId = $(".topics-list.edit li:first-child").data('topic-id');
+
+		projectTopics.functions.updateProjectPrimaryTopic(projectId, topicId);
+	});
+
 	// Add new topic on COMMA press
-	$(projectTopics.Selectors_.EDIT_ADD_TOPIC_INPUT).keypress(function(e) {
-		if((e.which == 44 || e.keyCode == 188) && $(this).val() != "") {
+	$(projectTopics.Selectors_.EDIT_ADD_TOPIC_INPUT).keypress(function (e) {
+		if ((e.which == 44 || e.keyCode == 188) && $(this).val() != "") {
 			var projectId = $(projectTopics.Selectors_.EDIT_PROJECT_FORM).data('project-id');
 			projectTopics.functions.addTopicToProject(projectId, $(this).val());
 		}
 	});
 
 	// Add new topic on input blur
-	$(projectTopics.Selectors_.EDIT_ADD_TOPIC_INPUT).on('blur', function(e) {
-		if($(this).val() != "") {
+	$(projectTopics.Selectors_.EDIT_ADD_TOPIC_INPUT).on('blur', function (e) {
+		if ($(this).val() != "") {
 			var projectId = $(projectTopics.Selectors_.EDIT_PROJECT_FORM).data('project-id');
 			projectTopics.functions.addTopicToProject(projectId, $(this).val());
 		}
 	});
 
 	// Remove topic
-	$('.topics-list.edit').on('click', '.topic .topic-remove', function(){
+	$('.topics-list.edit').on('click', '.topic .topic-remove', function () {
 		var projectId = $(projectTopics.Selectors_.EDIT_PROJECT_FORM).data('project-id');
 		var topicId = $(this).parent('li').data('topic-id');
 		projectTopics.functions.removeTopicFromProject(projectId, topicId);
 	});
 
-	$(projectTopics.Selectors_.EDIT_NEW_TOPIC_INPUT_CONTAINER).on('click', function() {
+	$(projectTopics.Selectors_.EDIT_NEW_TOPIC_INPUT_CONTAINER).on('click', function () {
 		$(projectTopics.Selectors_.EDIT_ADD_TOPIC_INPUT).focus();
 	});
 
 	/* ========
 		OTHER
 	=========== */
-	$('.supervisor-table').on('click', '.offer-action', function() {
+	$('.supervisor-table').on('click', '.offer-action', function () {
 		var actionButton = $(this);
 		var actionType = actionButton.data('action-type');
 		var tableRow = actionButton.parents().eq(1);
 
 		actionButton.html('<div class="spinner spinner-border spinner-border-sm"></div>');
 
-		if(actionType === "accept"){
+		if (actionType === "accept") {
 			var ajaxUrl = 'supervisor/student-accept';
-		} else if (actionType === "reject"){
+		} else if (actionType === "reject") {
 			var ajaxUrl = 'supervisor/student-reject';
 		}
 
-		if(ajaxUrl == null){
+		if (ajaxUrl == null) {
 			console.error("Invalid supervisor action.");
 			return;
 		}
@@ -279,22 +285,21 @@ import Swappable from '@shopify/draggable/lib/swappable';
 			method: 'POST',
 			url: ajaxUrl,
 			data: {
-				project_id : tableRow.data('project-id'),
-				student_id : tableRow.data('student-id')
+				project_id: tableRow.data('project-id'),
+				student_id: tableRow.data('student-id')
 			},
-			success: function(response){
-				if(response.successful){
-					tableRow.hide(400, function() { tableRow.remove(); });
+			success: function (response) {
+				if (response.successful) {
+					tableRow.hide(400, function () { tableRow.remove(); });
 
-					if(actionType === "accept"){
+					if (actionType === "accept") {
 						createToast('success', response.message);
 						updateAcceptedStudentsTable();
-					} else if (actionType === "reject"){
+					} else if (actionType === "reject") {
 						createToast('', response.message);
 					}
 
-					if(response.email_successful === false)
-					{
+					if (response.email_successful === false) {
 						var message = "The student was successful " + actionType + "ed. However, the confirmation email failed to send. We recommend you send one to them manually.";
 
 						$.confirm({
@@ -302,7 +307,7 @@ import Swappable from '@shopify/draggable/lib/swappable';
 							icon: '<div class="svg-md"><div class="svg-container"><svg viewBox="0 0 24 24"><path d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z" /></svg></div></div>',
 							theme: 'modern',
 							escapeKey: true,
-							animateFromElement : false,
+							animateFromElement: false,
 							backgroundDismiss: true,
 							title: 'Email Error',
 							content: message,
@@ -319,16 +324,16 @@ import Swappable from '@shopify/draggable/lib/swappable';
 		});
 	});
 
-	$("body").on("change", ".supervisor-table .checkbox input", function() {
-		var select = function(dom){
+	$("body").on("change", ".supervisor-table .checkbox input", function () {
+		var select = function (dom) {
 			var status = dom.parents().eq(4).data('status');
 			var supervisorEmail = dom.parents().eq(4).data('supervisor-email');
 			var emailString = "mailto:" + supervisorEmail + "?bcc=";
 			var checkboxSelector = '.supervisor-table.' + status + ' .checkbox input';
 			var emailButtonselector = ".email-selected." + status;
 
-			$(checkboxSelector).each(function(index, value) {
-				if($(value).is(":checked") && !$(value).hasClass("master-checkbox")) {
+			$(checkboxSelector).each(function (index, value) {
+				if ($(value).is(":checked") && !$(value).hasClass("master-checkbox")) {
 					emailString += $(value).data('email');
 					emailString += ",";
 				}
@@ -339,7 +344,7 @@ import Swappable from '@shopify/draggable/lib/swappable';
 		setTimeout(select($(this)), 2000);
 	});
 
-	$('#supervisor-accepted-students-table, #ProjectTableOverview').on('click', '.supervisor-undo-accept', function(e) {
+	$('#supervisor-accepted-students-table, #ProjectTableOverview').on('click', '.supervisor-undo-accept', function (e) {
 		var tableRow = $(this);
 		var studentName = tableRow.data('student-name');
 		var projectTitle = tableRow.data('project-title');
@@ -351,36 +356,36 @@ import Swappable from '@shopify/draggable/lib/swappable';
 			theme: 'modern',
 			escapeKey: true,
 			backgroundDismiss: true,
-			animateFromElement : false,
+			animateFromElement: false,
 			content: 'Are you sure you want to un-accept <b>' + studentName + '</b> for <b>' + projectTitle + '</b> ?',
 			buttons: {
 				confirm: {
 					btnClass: 'btn-red',
-					action: function(){
+					action: function () {
 						$.ajax({
 							method: 'PATCH',
 							url: 'admin/student-undo',
 							data: {
-								project_id : tableRow.data('project-id'),
-								student_id : tableRow.data('student-id')
+								project_id: tableRow.data('project-id'),
+								student_id: tableRow.data('student-id')
 							},
-							success:function(response){
-								if(response.successful){
-									tableRow.hide(400, function() { tableRow.remove(); });
+							success: function (response) {
+								if (response.successful) {
+									tableRow.hide(400, function () { tableRow.remove(); });
 									createToast('success', response.message);
 									updateAcceptedStudentsTable();
 								} else {
 									createToast('error', response.message);
 								}
 
-								if(!response.email_successful){
+								if (!response.email_successful) {
 									var message = "The undo action was successful. However, the confirmation email failed to send. We recommend you send one to them manually.";
 									$.confirm({
 										type: 'red',
 										icon: '<div class="svg-md"><div class="svg-container"><svg viewBox="0 0 24 24"><path d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z" /></svg></div></div>',
 										theme: 'modern',
 										escapeKey: true,
-										animateFromElement : false,
+										animateFromElement: false,
 										backgroundDismiss: true,
 										title: 'Email Error',
 										content: message,
@@ -398,11 +403,11 @@ import Swappable from '@shopify/draggable/lib/swappable';
 		});
 	});
 
-	function updateAcceptedStudentsTable(){
+	function updateAcceptedStudentsTable() {
 		$.ajax({
 			method: 'GET',
 			url: 'supervisor/accepted-students-table',
-			success: function(data){
+			success: function (data) {
 				$("#supervisor-accepted-students-table").html(data);
 			}
 		});
