df77ddf2c63ec11c5b046c43ef645a803afa2d87
Lewis Johnson <LewisJohnsonDev@gmail.com>
2022-04-18 20:26:05 +0100

    Delete project modal is now titled appropriately

--- app/resources/assets/js/main.js
+++ app/resources/assets/js/main.js
@@ -231,7 +231,7 @@ $(document).ajaxError(function (event, request, settings) {
 		var projectName = form.data('project-title');
 
 		$.confirm({
-			title: 'Delete',
+			title: 'Delete Project',
 			type: 'red',
 			icon: '<div class="svg-md"><div class="svg-container"><svg viewBox="0 0 24 24"><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg></div></div>',
 			theme: 'modern',
